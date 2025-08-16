6266661c7d32cb868a2bcc6a1c7bea6b6e450098
Haitham-sh <haithamelsherbiny81@gmail.com>
2025-08-16 23:58:23 +0300

    project A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS Customers (
 
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id FOREIGN KEY REFERENCES customers(id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE NOT NULL,
 );
 
