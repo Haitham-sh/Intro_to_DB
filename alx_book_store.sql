c1657cd969e16f70152dc0f5cc9519fc08a1a45f
Haitham-sh <haithamelsherbiny81@gmail.com>
2025-08-16 23:53:18 +0300

    project A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id FOREIGN KEY REFERENCES authors(id),
@@ -10,25 +10,25 @@ CREATE TABLE IF NOT EXISTS books (
     publication_date DATE,
 );
 
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL,
 );
 
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
     address TEXT
 );
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id FOREIGN KEY REFERENCES customers(id),
     order_date DATE NOT NULL,
 );
 
-CREATE TABLE IF NOT EXISTS order_detail (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id FOREIGN KEY REFERENCES orders(id),
     book_id FOREIGN KEY REFERENCES books(id),
